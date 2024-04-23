c03d9f083db23586cd5f5713fbf1ace1b0f11257
LHackSRT <bambabar02@gmail.com>
2024-04-23 11:47:35 +0000

    feature

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,13 +1,10 @@
-import {useState} from "react";
 
 
 export const Pagination = ({page, length, dataCount} : any) => {
     
     const nbToRender = 10;
-    const [pageList, setPageList] = useState(5);
-    const setPage = (p: any) => {
-        console.log(p);
-        setPageList(p);
+    const pageList = 0;
+    const setPage = () => {
     }
 
     return(<>
@@ -17,7 +14,7 @@ export const Pagination = ({page, length, dataCount} : any) => {
                         className="font-semibold text-gray-900 ">{length}</span></span>
             <ul className="inline-flex items-center -space-x-px">
                 <li>
-                    <button onClick={() => {if (page > 0) setPage((p: any) => (p - 1))}}
+                    <button onClick={() => {if (page > 0) setPage()}}
                             className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">
                         <span className="sr-only">Previous</span>
                         <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
@@ -29,7 +26,7 @@ export const Pagination = ({page, length, dataCount} : any) => {
                     </button>
                 </li>
                 <li ><button onClick={() => {
-                    setPage(0)
+                    setPage()
                 }} className={ `px-3 py-2 leading-tight border ${page !== 0 ? " text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700" : "z-10 text-blue-600 border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"}`}>
                     {1}
                 </button></li>
@@ -37,13 +34,13 @@ export const Pagination = ({page, length, dataCount} : any) => {
                     <button className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ">...</button>
                 </li>}
                 {  Array.from (Array(pageList), ((a:any,index) => {
+                    console.log(a);
                     if (index + page - (pageList - 1) / 2 > 0 && index + page - (pageList - 1) / 2 < Math.floor(dataCount / nbToRender) - 1)
-                        console.log(a);
                         return (
                             <li key={index}>
                                 <button onClick={() => {
                                     if (page !== index + page - (pageList - 1) / 2)
-                                        setPage(index + page - (pageList - 1) / 2)
+                                        setPage()
                                 }} className={ `px-3 py-2 leading-tight border ${page !== index + page - (pageList - 1) / 2 ? " text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700" : "z-10 text-blue-600 border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"}`}>
                                     {index + 1 - (pageList - 1) / 2 + page}</button>
                             </li>
@@ -55,14 +52,14 @@ export const Pagination = ({page, length, dataCount} : any) => {
                 </li>}
                 {Math.floor(dataCount / nbToRender) > 0 &&<li>
                     <button onClick={() => {
-                        setPage(Math.floor(dataCount / nbToRender) - 1)
+                        setPage()
                     }}
                             className={`px-3 py-2 leading-tight border ${page !== Math.floor(dataCount / nbToRender) - 1 ? " text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700" : "z-10 text-blue-600 border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"}`}>
                         {Math.floor(dataCount / nbToRender)}
                     </button>
                 </li>}
                 <li>
-                    <button onClick={() => {if (page < Math.floor(dataCount / nbToRender) ) setPage((p: any) => (p + 1))}}
+                    <button onClick={() => {if (page < Math.floor(dataCount / nbToRender) ) setPage()}}
                             className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">
                         <span className="sr-only">Next</span>
                         <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
